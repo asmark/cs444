bc3b03c4de735f8e612d693100d6a08bfd3c95c3
asmark <asmar.k21@gmail.com>
2014-02-11 12:00:16 -0500

    Statement types

--- common/src/main/scala/joos/ast/AstNode.scala
+++ common/src/main/scala/joos/ast/AstNode.scala
@@ -1,5 +1,5 @@
 package joos.ast
 
 trait AstNode {
-
+  val parent : Option[AstNode]
 }
--- common/src/main/scala/joos/ast/Statement.scala
+++ common/src/main/scala/joos/ast/Statement.scala
@@ -3,3 +3,20 @@ package joos.ast
 trait Statement extends AstNode {
 
 }
+
+case class WhileStatement(cond: Expression, body: Statement)
+
+case class ForStatement(forInit: Expression, cond: Expression, forUpdate: Expression, body: Statement)
+
+case class IfStatement(test: Expression, tStatement: Statement, fStatement: Statement)
+
+case class ExpressionStatement(expr: Expression)
+
+case class ReturnStatement(exp: Expression)
+
+case class TypeDeclarationStatement(decl: Declaration)
+
+case class EmptyStatement()
+
+
+
