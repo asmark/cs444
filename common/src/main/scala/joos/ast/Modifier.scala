8cdd1cdc409fa2be5776fa0cf5d340ba8a18b878
asmark <asmar.k21@gmail.com>
2014-02-17 20:19:18 -0500

    AST Modifiers node construction

--- common/src/main/scala/joos/ast/Modifier.scala
+++ common/src/main/scala/joos/ast/Modifier.scala
@@ -1,12 +1,20 @@
 package joos.ast
 
-import joos.parsetree.ParseTreeNode
-import joos.tokens.TerminalToken
+import joos.parsetree.{LeafNode, TreeNode, ParseTreeNode}
+import joos.tokens.{TokenKind, TerminalToken}
+import joos.language.ProductionRule
 
 case class Modifier(modifier: TerminalToken) extends AstNode
 
 object Modifier {
-  def apply(ptn: ParseTreeNode) = {
-    null
+  def apply(ptn: ParseTreeNode): Seq[Modifier] = {
+    ptn match {
+      case TreeNode(ProductionRule("Modifiers", Seq("Modifiers", "Modifier")), _, children) =>
+        return Modifier(children(0)) ++ Modifier(children(1))
+      case TreeNode(ProductionRule("Modifiers", Seq("Modifier")), _, children) =>
+        return Modifier(children(0))
+      case TreeNode(ProductionRule("Modifier", Seq(_)), _, Seq(LeafNode(token))) =>
+        return Seq(Modifier(token))
+    }
   }
 }
