6cc3096f61cb604bec1c0f06e1570e9d6b910a40
ShengMin Zhang <me@shengmin.me>
2014-03-01 20:30:02 -0500

    Update logger

--- common/src/main/scala/joos/core/Logger.scala
+++ common/src/main/scala/joos/core/Logger.scala
@@ -1,24 +1,37 @@
 package joos.core
 
 object Logger {
+  private[this] val LogOff = Int.MaxValue
+  private[this] val LogError = 3
+  private[this] val LogWarning = 2
+  private[this] val LogInformation = 1
+  private[this] val LogLevel = LogError
+
+
   def logError(text: String): this.type = {
-    Console.err.print(Console.RED)
-    Console.err.println(text)
-    Console.err.print(Console.RESET)
+    if (LogLevel <= LogError) {
+      Console.err.print(Console.RED)
+      Console.err.println(text)
+      Console.err.print(Console.RESET)
+    }
     this
   }
 
   def logWarning(text: String): this.type = {
-    Console.err.print(Console.YELLOW)
-    Console.err.println(text)
-    Console.err.print(Console.RESET)
+    if (LogLevel <= LogWarning) {
+      Console.err.print(Console.YELLOW)
+      Console.err.println(text)
+      Console.err.print(Console.RESET)
+    }
     this
   }
 
   def logInformation(text: String): this.type = {
-    Console.err.print(Console.GREEN)
-    Console.err.println(text)
-    Console.err.print(Console.RESET)
+    if (LogLevel <= LogInformation) {
+      Console.err.print(Console.BLUE)
+      Console.err.println(text)
+      Console.err.print(Console.RESET)
+    }
     this
   }
 }
