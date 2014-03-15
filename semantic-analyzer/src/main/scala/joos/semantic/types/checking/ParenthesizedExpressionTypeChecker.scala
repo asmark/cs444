81f051453c4ea150b4396a36b5282c5694ee16f2
ShengMin Zhang <me@shengmin.me>
2014-03-15 03:54:15 -0400

    Add the check that no type can be parenthesized

--- semantic-analyzer/src/main/scala/joos/semantic/types/checking/ParenthesizedExpressionTypeChecker.scala
+++ semantic-analyzer/src/main/scala/joos/semantic/types/checking/ParenthesizedExpressionTypeChecker.scala
@@ -1,13 +1,25 @@
 package joos.semantic.types.checking
 
+import joos.ast.expressions.{SimpleNameExpression, ParenthesizedExpression}
 import joos.ast.visitor.AstVisitor
-import joos.ast.expressions.ParenthesizedExpression
+import joos.semantic.types.TypeCheckingException
 
 trait ParenthesizedExpressionTypeChecker extends AstVisitor {
   self: TypeChecker =>
-  override def apply(parenthesizedExpression: ParenthesizedExpression) {
-    parenthesizedExpression.expression.accept(this)
-    require(parenthesizedExpression.expression.declarationType != null)
-    parenthesizedExpression.declarationType = parenthesizedExpression.expression.declarationType
+
+  override def apply(parenthesis: ParenthesizedExpression) {
+    parenthesis.expression.accept(this)
+    require(parenthesis.expression.declarationType != null)
+
+    // Je_1_Dot_ParenthesizedType_Field
+    parenthesis.expression match {
+      case name: SimpleNameExpression =>
+        // This has to refer to a local variable/instance field
+        if (!blockEnvironment.contains(name) && !typeEnvironment.containedFields.contains(name)) {
+          throw new TypeCheckingException("parenthesis", s"${name} needs to be a local variable or instance field")
+        }
+    }
+
+    parenthesis.declarationType = parenthesis.expression.declarationType
   }
 }
