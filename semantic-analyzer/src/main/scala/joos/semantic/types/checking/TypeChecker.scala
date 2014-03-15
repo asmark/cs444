8092ea58915eb4b020747e41093776ac5400b8ea
ShengMin Zhang <me@shengmin.me>
2014-03-15 00:11:36 -0400

    Fix this type check

--- semantic-analyzer/src/main/scala/joos/semantic/types/checking/TypeChecker.scala
+++ semantic-analyzer/src/main/scala/joos/semantic/types/checking/TypeChecker.scala
@@ -1,24 +1,24 @@
 package joos.semantic.types.checking
 
 import joos.ast.declarations.{TypeDeclaration, MethodDeclaration, FieldDeclaration}
-import joos.ast.visitor.AstCompleteVisitor
 import joos.ast.{Modifier, CompilationUnit}
 import joos.semantic._
 import joos.semantic.types._
 
-class TypeChecker(implicit val unit: CompilationUnit) extends AstEnvironmentVisitor
-with AssignmentExpressionTypeChecker
-with ArrayAccessExpressionTypeChecker
-with ArrayCreationExpressionTypeChecker
-with CastExpressionTypeChecker
-with ClassInstanceCreationExpressionTypeChecker
-with FieldAccessExpressionTypeChecker
-with InfixExpressionTypeChecker
-with MethodInvocationExpressionTypeChecker
-with ParenthesizedExpressionTypeChecker
-with ThisExpressionTypeChecker
-with VariableDeclarationExpressionTypeChecker
-with PrefixExpressionTypeChecker {
+class TypeChecker(implicit val unit: CompilationUnit)
+    extends AstEnvironmentVisitor
+    with AssignmentExpressionTypeChecker
+    with ArrayAccessExpressionTypeChecker
+    with ArrayCreationExpressionTypeChecker
+    with CastExpressionTypeChecker
+    with ClassInstanceCreationExpressionTypeChecker
+    with FieldAccessExpressionTypeChecker
+    with InfixExpressionTypeChecker
+    with MethodInvocationExpressionTypeChecker
+    with ParenthesizedExpressionTypeChecker
+    with ThisExpressionTypeChecker
+    with VariableDeclarationExpressionTypeChecker
+    with PrefixExpressionTypeChecker {
   var checkImplicitThis = false // TODO: this approach should be fine as long as the field decl checking and method decl checking are not recursive
 
   override def apply(fieldDeclaration: FieldDeclaration) {
