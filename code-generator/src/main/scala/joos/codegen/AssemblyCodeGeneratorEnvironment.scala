d96ef977c006efa65a2c83a612141b40bd75a95b
asmark <asmar.k21@gmail.com>
2014-03-31 14:08:35 -0400

    Add Common Library

--- code-generator/src/main/scala/joos/codegen/AssemblyCodeGeneratorEnvironment.scala
+++ code-generator/src/main/scala/joos/codegen/AssemblyCodeGeneratorEnvironment.scala
@@ -1,6 +1,57 @@
 package joos.codegen
 
+import AssemblyCodeGeneratorEnvironment._
+import java.io.{File, PrintWriter}
+import joos.assemgen._
+import joos.ast.AbstractSyntaxTree
+import joos.codegen.generators.TypeDeclarationCodeGenerator
+
 /**
  * Stores the environment passed to code generator
  */
-class AssemblyCodeGeneratorEnvironment(val assemblyManager: AssemblyFileManager)
+class AssemblyCodeGeneratorEnvironment(val assemblyManager: AssemblyFileManager, val namespace: AssemblyNamespace) {
+  def this(ast: AbstractSyntaxTree, namespace: AssemblyNamespace) = {
+    this(new AssemblyFileManager(s"${ast.name}.s"), namespace)
+
+    implicit val environment = this
+    ast.root.typeDeclaration match {
+      case None =>
+      case Some(typeDeclaration) => new TypeDeclarationCodeGenerator(typeDeclaration).generate
+    }
+  }
+
+  def write {
+    val writer = new PrintWriter(new File(s"${OutputDirectory}/${assemblyManager.fileName}"))
+
+    writer.print(comment(sectionFormatString.format("Text")))
+    writer.print(section(AssemblySection.Text))
+
+    writer.print(comment("Defining Exported Symbols"))
+    assemblyManager.globals.foreach(global => writer.print(global))
+    writer.print(emptyLine)
+
+    writer.print(comment("Defining Imported Symbols"))
+    namespace.externs.foreach(extern => writer.print(extern))
+    writer.print(emptyLine)
+
+    writer.print(comment("Defining body"))
+    writer.print(emptyLine)
+
+    assemblyManager.text.foreach(writer.print)
+    writer.print(emptyLine)
+
+    writer.print(comment(sectionFormatString.format("Data")))
+    writer.print(section(AssemblySection.Data))
+    writer.print(emptyLine)
+
+    // TODO: What to print here?
+
+    writer.flush()
+    writer.close()
+  }
+}
+
+object AssemblyCodeGeneratorEnvironment {
+  private val sectionFormatString = "--- %s ---"
+  private val OutputDirectory = "output"
+}
--- compiler/src/main/scala/joos/codegen/CodeGeneration.scala
+++ compiler/src/main/scala/joos/codegen/CodeGeneration.scala
@@ -1,27 +1,22 @@
 package joos.codegen
 
-import java.io.{File, PrintWriter}
 import joos.ast.AbstractSyntaxTree
-import joos.ast.declarations.ModuleDeclaration
-import joos.codegen.generators.AssemblyCodeGenerator
+import joos.codegen.commonlib.AssemblyCommonLibraryEnvironment
 
 object CodeGeneration {
 
-  final val OutputDirectory = "output"
-
-  def getAssemblyManager(ast: AbstractSyntaxTree) = AssemblyFileManager(new PrintWriter(new File(s"${OutputDirectory}/${ast.name}.s")))
-
   def apply(asts: Seq[AbstractSyntaxTree]) {
-    implicit val module = new ModuleDeclaration
 
-    // TODO: Link in a global namespace to AssemblyCodeGeneratorEnvironment
+    val namespace = new AssemblyNamespace
+    val CommonLibrary = AssemblyCommonLibraryEnvironment(namespace)
 
-    asts.foreach {
-      ast =>
-        implicit val environment = new AssemblyCodeGeneratorEnvironment(getAssemblyManager(ast))
-        AssemblyCodeGenerator(ast)
-        environment.assemblyManager.print()
+    val assemblyEnvironments = asts.foldRight(Seq(CommonLibrary)) {
+      (ast, environments) =>
+        environments :+ new AssemblyCodeGeneratorEnvironment(ast, namespace)
     }
+
+    assemblyEnvironments foreach (_.write)
+
   }
 
 }
