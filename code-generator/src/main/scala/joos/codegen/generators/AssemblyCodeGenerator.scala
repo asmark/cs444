e4b17698c3aefe490297390936f5473b32b3e84d
ShengMin Zhang <me@shengmin.me>
2014-03-31 00:12:50 -0400

    WIP

--- code-generator/src/main/scala/joos/codegen/generators/AssemblyCodeGenerator.scala
+++ code-generator/src/main/scala/joos/codegen/generators/AssemblyCodeGenerator.scala
@@ -1,7 +1,8 @@
 package joos.codegen.generators
 
 import joos.assemgen.AssemblyLine
-import joos.ast.AstNode
+import joos.ast.expressions._
+import joos.ast.{ExpressionDispatcher, AstNode}
 import joos.codegen.AssemblyCodeGeneratorEnvironment
 import scala.language.implicitConversions
 
@@ -20,6 +21,51 @@ trait AssemblyCodeGenerator {
 }
 
 object AssemblyCodeGenerator {
+  implicit def toAssemblyCodeGenerator(expression: Expression)
+      (implicit environment: AssemblyCodeGeneratorEnvironment): AssemblyCodeGenerator = {
+    var generator: AssemblyCodeGenerator = null
+    val creator = new ExpressionDispatcher {
+      override def apply(expression: ArrayAccessExpression) {
+
+      }
+
+      override def apply(expression: ArrayCreationExpression): Unit = super.apply(expression)
+
+      override def apply(expression: AssignmentExpression): Unit = super.apply(expression)
+
+      override def apply(expression: BooleanLiteral) = generator = new BooleanLiteralCodeGenerator(expression)
+
+      override def apply(expression: CastExpression): Unit = super.apply(expression)
+
+      override def apply(expression: CharacterLiteral): Unit = super.apply(expression)
+
+      override def apply(expression: ClassInstanceCreationExpression): Unit = super.apply(expression)
+
+      override def apply(expression: FieldAccessExpression): Unit = super.apply(expression)
+
+      override def apply(expression: InfixExpression): Unit = super.apply(expression)
+
+      override def apply(expression: InstanceOfExpression): Unit = super.apply(expression)
+
+      override def apply(expression: IntegerLiteral): Unit = super.apply(expression)
+
+      override def apply(expression: MethodInvocationExpression): Unit = super.apply(expression)
+
+      override def apply(expression: NullLiteral): Unit = super.apply(expression)
+
+      override def apply(expression: ParenthesizedExpression): Unit = super.apply(expression)
+
+      override def apply(expression: PrefixExpression): Unit = super.apply(expression)
+
+      override def apply(expression: VariableDeclarationExpression): Unit = super.apply(expression)
+
+      override def apply(expression: ThisExpression): Unit = super.apply(expression)
+    }
+
+    creator(expression)
+    generator
+  }
+
   implicit def toAssemblyCodeGenerator(element: AstNode)
       (implicit environment: AssemblyCodeGeneratorEnvironment): AssemblyCodeGenerator = {
     // TODO: implement
