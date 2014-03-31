adc9ddfb2232bee40cb53cb66709d8ac26d3d6e5
asmark <asmar.k21@gmail.com>
2014-03-31 15:33:46 -0400

    Add - to assembly expression

--- code-generator/src/main/scala/joos/assemgen/AssemblyExpression.scala
+++ code-generator/src/main/scala/joos/assemgen/AssemblyExpression.scala
@@ -1,15 +1,27 @@
 package joos.assemgen
 
-import joos.core.Writable
 import java.io.PrintWriter
+import joos.core.Writable
 
 trait AssemblyExpression extends Writable {
+  
   def +(other: AssemblyExpression) = {
     val that = this
     new AssemblyExpression {
       override def write(writer: PrintWriter) {
         that.write(writer)
         writer.write(" + ")
+        other.write(writer)
+      }
+    }
+  }
+
+  def -(other: AssemblyExpression) = {
+    val that = this
+    new AssemblyExpression {
+      override def write(writer: PrintWriter) {
+        that.write(writer)
+        writer.write(" - ")
         other.write(writer)
       }
     }
