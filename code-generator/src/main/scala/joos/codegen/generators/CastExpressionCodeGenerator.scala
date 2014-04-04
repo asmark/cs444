7c9c2d9f425f6992e9996c44a9f97106647bd8d1
freddiefeng <freddie_feng@hotmail.com>
2014-04-04 15:00:01 -0400

    Array Casting and InstaceOf

--- code-generator/src/main/scala/joos/codegen/generators/CastExpressionCodeGenerator.scala
+++ code-generator/src/main/scala/joos/codegen/generators/CastExpressionCodeGenerator.scala
@@ -7,45 +7,53 @@ import joos.core.Logger
 import joos.assemgen._
 import joos.assemgen.Register._
 
-class CastExpressionCodeGenerator(expression: CastExpression)
+class CastExpressionCodeGenerator(castExpression: CastExpression)
     (implicit val environment: AssemblyCodeGeneratorEnvironment) extends AssemblyCodeGenerator {
 
+  // CastExpression := (leftType) rightType
   override def generate() {
-    expression.expression.generate()
+    castExpression.expression.generate()
 
-    expression.expression.expressionType match {
-      case leftType: SimpleType => {
+    castExpression.expression.expressionType match {
+      case rightType: SimpleType => {
 
-        expression.castType match {
-          case rightType : SimpleType => {
-            val subtypeIndex = environment.staticDataManager.getTypeIndex(rightType.declaration)
+        castExpression.castType match {
+          case leftType : SimpleType => {
+            val subtypeIndex = environment.staticDataManager.getTypeIndex(leftType.declaration)
+            val endLabel = nextLabel("is_null_object")
             appendText(
-              :#(s"[BEGIN] Cast Expression ${expression}"),
+              :#(s"[BEGIN] Cast Expression ${castExpression}"),
+              cmp(Eax, 0),
+              je(endLabel),
               mov(Ebx, at(Eax+SubtypeTableOffset)) :# "Move subtype table of left into ebx",
               mov(Ebx, at(Ebx + 4 * subtypeIndex)) :# "Look up value in subtype table for instance check",
               cmp(Ebx, 0) :# "Throw exception if it cannot be cast",
-              je(exceptionLabel)
-              :#(s"[END] Cast Expression ${expression}")
+              je(exceptionLabel),
+              endLabel::,
+              :#(s"[END] Cast Expression ${castExpression}")
             )
           }
 
-          case rightType : PrimitiveType => {
+          case leftType : PrimitiveType => {
             // Not sure if we need this
             appendText(je(exceptionLabel))
-            Logger.logInformation(s"Exception when casting object type to primitive type in ${expression}")
+            Logger.logInformation(s"Exception when casting object type to primitive type in ${castExpression}")
           }
 
+          case NullType => {}
+
           case x => {
-            Logger.logWarning(s"No support to cast ${leftType} as ${x} in ${expression}")
+            Logger.logWarning(s"No support to cast ${rightType} as ${x} in ${castExpression}")
           }
 
         }
 
       }
 
-      case leftType: PrimitiveType => {
+      // We don't need runtime processing as no narrowing is allowed for primitive
+      case rightType: PrimitiveType => {
 
-        expression.castType match {
+        castExpression.castType match {
           case rightType: PrimitiveType => {
 
           }
@@ -53,107 +61,61 @@ class CastExpressionCodeGenerator(expression: CastExpression)
 
           }
         }
-
-
       }
 
+      case rightType: ArrayType => {
+
+        castExpression.castType match {
+          case ArrayType(rightElementType, _) => {
+            val subtypeIndex = rightElementType match {
+              case rightSimpleType: SimpleType => {
+                environment.staticDataManager.getArrayTypeIndex(rightSimpleType.declaration)
+              }
+              case rightPrimitiveType: PrimitiveType => {
+                environment.staticDataManager.getArrayTypeIndex(rightPrimitiveType)
+              }
+            }
+
+            generateObjectCheck(subtypeIndex)
+          }
+          case rightType: SimpleType => {
+            // Castable to Object, Cloneable and Serializable.
+            val subtypeIndex = environment.staticDataManager.getTypeIndex(rightType.declaration)
+            generateObjectCheck(subtypeIndex)
+          }
+          case x =>
+            Logger.logWarning(s"No support to cast ${x} in ${castExpression}")
+        }
+      }
+
+      case NullType => {}
+
       case x =>
-        Logger.logWarning(s"No support to cast ${x} in ${expression}")
+        Logger.logWarning(s"No support to cast ${x} in ${castExpression}")
     }
-
-
-
-    //    // EAX should have a reference to the object/class etc
-    //
-    //    expression.castType match {
-    //      case PrimitiveType.ByteType => {
-    //        expression.castType match {
-    //          case PrimitiveType.ShortType | PrimitiveType.IntegerType=> {
-    //            appendText(:#("[BEGIN] Casting to Byte"))
-    //            prologue(0)
-    //            appendText(
-    //              mov(Ebx, toExpression(255)),
-    //              and(Eax, Ebx)
-    //            )
-    //            epilogue(0)
-    //            appendText(:#("[END] Casting to Byte"))
-    //          }
-    //          case _ =>
-    //        }
-    //      }
-    //      case PrimitiveType.ShortType => {
-    //        expression.castType match {
-    //          case PrimitiveType.IntegerType => {
-    //            appendText(:#("[BEGIN] Casting Int to Short"))
-    //            prologue(0)
-    //            appendText(
-    //              mov(Ebx, toExpression(255)),
-    //              and(Eax, Ebx)
-    //            )
-    //            epilogue(0)
-    //            appendText(:#("[END] Casting Int to Short"))
-    //          }
-    //          case _ => {}
-    //        }
-    //      }
-    //      case PrimitiveType.CharType => {} // Place holder
-    //      case dst: SimpleType => {
-    //        expression.expression.expressionType match {
-    //          case ArrayType(_,_) => {
-    //            require(dst.declaration.fullName == javaLangObject.standardName ||
-    //                dst.declaration.fullName == javaLangObject.standardName ||
-    //                dst.declaration.fullName == javaIOSerializable.standardName)
-    //          }
-    //          case NullType => {
-    //            //TODO: Do nothing?
-    //
-    //          }
-    //          case src: SimpleType => {
-    //            val dstTypeDeclration = environment.typeEnvironment.compilationUnit.getVisibleType(dst.name)
-    //            require(dstTypeDeclration.isDefined)
-    //
-    //            val dstSit = selectorTableLabel(dstTypeDeclration.get)
-    //            val dstSubTypeTable = subtypeTableLabel(dstTypeDeclration.get)
-    //            val dstTypeOffset = environment.staticDataManager.getTypeIndex(dstTypeDeclration.get) * 4
-    //
-    //            val validLabel = "valid" + DefaultUniqueIdGenerator.nextId()
-    //            val endLabel = "end" + DefaultUniqueIdGenerator.nextId()
-    //
-    //            appendText(:#(s"[BEGIN] Casting ${src.standardName} to ${dst.standardName}"))
-    //            prologue(0)
-    //            appendText(
-    //              #>,
-    //              push(Eax) :#"Save EAX",
-    //              mov(Ebx, Eax),
-    //              add(Ebx, toExpression(SelectorTableOffset)),
-    //              mov(Ebx, at(Ebx)) :#"EBX should point to the sub type table",
-    //              add(Ebx, dstTypeOffset),
-    //              mov(Ebx, at(Ebx)) :#"Look up value in the subtype table",
-    //              cmp(Ebx, toExpression(1)),
-    //              je(labelReference(validLabel)),
-    //              call(labelReference(exceptionLabel)) :#"Cast error",
-    //              jmp(labelReference(endLabel)),
-    //              validLabel ::,
-    //              mov(Ebx, Eax) :#"Update selector table",
-    //              add(Ebx, toExpression(SelectorTableOffset)),
-    //              movdw(at(Ebx), labelReference(dstSit)),
-    //              add(Ebx, toExpression(SubtypeTableOffset - SelectorTableOffset)) :#"Update sub type table",
-    //              movdw(at(Ebx), labelReference(dstSubTypeTable)),
-    //              endLabel ::,
-    //              pop(Eax) :#"Restore EAX",
-    //              #<
-    //            )
-    //            epilogue(0)
-    //            appendText(:#(s"[END] Casting Casting ${src.standardName} to ${dst.standardName}"))
-    //          }
-    //          case _ => {}
-    //        }
-    //      }
-    //      case ArrayType(_,_) => {
-    //
-    //      }
-    //      case _ => {}
-    //    }
   }
 
+  private def generateObjectCheck(subtypeIndex: Int) {
+    appendText(
+      :#(s"[BEGIN] Cast check ${castExpression}"),
+      :#("Look up left hand side"),
+      #>
+    )
+
+    castExpression.expression.generate()
+
+    val endLabel = nextLabel("is_null_object")
+    appendText(
+      #<,
+      emptyLine,
+      cmp(Eax, 0),
+      je(endLabel),
+      mov(Ebx, at(Eax + SubtypeTableOffset)) :# "Put address of subtype table in eax",
+      mov(Ebx, at(Ebx + 4 * subtypeIndex)) :# "Look up value in subtype table for instance check",
+      cmp(Ebx, 0) :# "Throw exception if it cannot be cast",
+      je(exceptionLabel),
+      endLabel::,
+      :#(s"[END] Cast check ${castExpression}")
+    )
+  }
 }
