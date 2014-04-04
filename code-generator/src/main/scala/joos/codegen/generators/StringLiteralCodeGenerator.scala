5dcd4a520d854f54a09d39efbf2545a647273c4e
shengmin <me@shengmin.me>
2014-04-04 02:29:43 -0400

    Add StringLiteral

--- code-generator/src/main/scala/joos/codegen/generators/StringLiteralCodeGenerator.scala
+++ code-generator/src/main/scala/joos/codegen/generators/StringLiteralCodeGenerator.scala
@@ -2,7 +2,10 @@ package joos.codegen.generators
 
 import joos.assemgen.Register._
 import joos.assemgen._
-import joos.ast.expressions.StringLiteral
+import joos.ast._
+import joos.ast.expressions.{ArrayCreationExpression, StringLiteral}
+import joos.ast.types.PrimitiveType._
+import joos.ast.types._
 import joos.codegen.AssemblyCodeGeneratorEnvironment
 
 class StringLiteralCodeGenerator(literal: StringLiteral)
@@ -12,10 +15,29 @@ class StringLiteralCodeGenerator(literal: StringLiteral)
   override def generate() {
     appendText(
       emptyLine,
-      :# (s"[BEGIN] Assemble string literal: ${literal}"),
-      literal.uniqueName :: db(literal.text),
-      mov(Eax, labelReference(literal.uniqueName)),
-      :# (s"[END] Assemble string literal: ${literal}"),
+      #>,
+      :#(s"[BEGIN] Create String Literal: ${literal}")
+    )
+
+    ArrayCreationExpression(CharType, literal.text.length).generate()
+    for (i <- 0 until literal.text.length) {
+      appendText(
+        movdw(at(Eax + (i * 4 + ArrayFirstElementOffset)), literal.text(i))
+      )
+    }
+
+    appendText(
+      :#("[BEGIN] Create String Object"),
+      push(Eax),
+      call(mallocTypeLabel(StringType.declaration)) :# "ecx = newly allocated object",
+      call(StringCharArrayConstructor.uniqueName) :# "Call String(char[])",
+      mov(Eax, Ecx),
+      :#("[END] Create String Object")
+    )
+
+    appendText(
+      :#(s"[END] Create String Literal: ${literal}"),
+      #<,
       emptyLine
     )
   }
